848d2d3a8e81f8fb8403be4b13f204dd44de6ac2
AMRR\amrmo <amohamed@sumerge.com>
2022-09-21 02:16:14 +0200

    adding react udemy project

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Course from './Components/pages/Course';
+import Home from './Components/pages/Home/index';
+import { CircularProgress } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route ,useParams ,Link} from 'react-router-dom';
+function App() {
+  const [courses,setCourses] = useState([]);
+  const [loading,setloading] = useState(1);
+  useEffect(() => {
+    setloading(1)
+    fetch(
+      "https://mocki.io/v1/9512ea8c-178c-4669-831a-cfb846061391")
+                  .then((res) => res.json())
+                  .then((json) => {
+                      
+                      setCourses(json);
+                      setloading(0)
+                  })
+    
+  },[])
+  function CoursePage() {
+    let { id } = useParams();
+    console.log( id );
+    let course = courses.courses
+    course = course.filter(e=>e.id == id);
+    if(course.length>0){
+      return <Course course={course} />;
+    }
+    else{
+      return <div className='flex justify-center text-4xl text-white-2 h-full bg-black'>404 Not Found <Link to='/' className='underline text-violet-700'><br></br> Go home?</Link></div>
+
+    }
+    
+  }
+  return (
+    <div className="App">
+    <Router>
+      <Routes>
+        <Route path={"/"} element={loading === 1 ? <div className='flex justify-center items-center'><CircularProgress size={100}/></div> : <Home courses={courses} />} />
+        <Route path={"/search=:text"} element={loading === 1 ? <div className='flex justify-center items-center'><CircularProgress size={100}/></div> : <Home courses={courses} />} />
+        <Route path={"/course/:id"} element={loading === 1 ? <div className='flex justify-center items-center'><CircularProgress size={100}/></div> : <CoursePage />} />
+        <Route path='*' element={<div className='flex justify-center text-4xl text-white-2 h-full bg-black'>404 Not Found <Link to='/' className='underline text-violet-700'><br></br> Go home?</Link></div>} />
+      </Routes>
+    </Router>
+      
+    </div>
+  );
+}
+
+export default App;
